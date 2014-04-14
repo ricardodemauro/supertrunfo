2c3ee436730bb36e5633b7aa28b39ab96adbd56b
ricardo.demauro <ricardo.demauro@gmail.com>
2014-04-13 23:38:21 -0300

    .

---
+++ SuperTrunfo.Servico.AMS/DataObjects/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace SuperTrunfo.Servico.AMS.DataObjects
+{
+    public class TodoItem : EntityData
+    {
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
